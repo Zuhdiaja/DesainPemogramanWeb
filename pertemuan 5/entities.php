2c6d5a039752681ea11fc091f186fc36e07f1584
Zuhdiaja <145771767+Zuhdiaja@users.noreply.github.com>
2025-10-01 01:48:08 +0700

    jobsheet4_praktikum 10-11

---
+++ pertemuan 5/entities.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Entities HTML</title>
+    </head>
+    <body>
+        <p>It&#39; time to read a HTML5 Book.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp; Keuntungan dari menggunakan nama entities:
+            Sebua nama entitas mudah diingat. Kerugian dari menggunakan nama entites:
+            Browser mungkin tidak mendukung semau entitas, terapi dukungan untuk nomer lebih baik.
+        </p>
+        <p>&#169; 2023 jti.com</p>
+    </body>
+</html>
